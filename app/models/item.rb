6261b8af3159c50f14778adfd106c136676ba051
IT0825 <ace0829.1114@icloud.com>
2021-08-12 16:48:44 +0900

    バリデーションとアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  
+  with_options presence: true do
+    validates :product_name
+    validates :description_of_item
+    validates :category_id
+    validates :product_condition_id
+    validates :shipping_fee_burden_id
+    validates :prefecture_id
+    validates :day_to_ship_id
+    validates :price
+  end
 end
