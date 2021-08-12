0a7775e7a1d87d50c5176ac56bda47ced4583561
IT0825 <ace0829.1114@icloud.com>
2021-08-13 08:37:38 +0900

    Sales profit作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -19,6 +18,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:product_name, :description_of_item,:category_id, :product_condition_id, :shipping_fee_burden_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :description_of_item, :category_id, :product_condition_id, :shipping_fee_burden_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
