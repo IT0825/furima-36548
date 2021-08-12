4403ae337c7a58fd1d413e89d367dd59a1cf0082
IT0825 <ace0829.1114@icloud.com>
2021-08-12 11:47:54 +0900

    モデル単体テストコード完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname 
+    nickname {'テスト'}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    last_name
-    first_name
+    last_name {'てすと'}
+    first_name {'てすと'}
     last_name_kana  {'テスト'}
     first_name_kana {'テスト'}
-    birth_date
+    birth_date {'2000-1-1'}
   end
 end
