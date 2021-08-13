72513fbd621c34e890dcd3b7c3b8b33eac6e3bd6
IT0825 <ace0829.1114@icloud.com>
2021-08-13 12:06:01 +0900

    FactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name           { 'テスト' }
+    description_of_item    { 'テスト' }
+    category_id            { 1 }
+    product_condition_id   { 1 }
+    shipping_fee_burden_id { 1 }
+    prefecture_id          { 1 }
+    day_to_ship_id         { 1 }
+    price                  { 300 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
