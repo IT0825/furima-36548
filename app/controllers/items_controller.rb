97d1591d6c56440f21b847e3ea0f2f600bfa7cfc
IT0825 <ace0829.1114@icloud.com>
2021-08-14 15:14:06 +0900

    修正箇所2点完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [ :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.user.id
-    else
-      redirect_to root_path
-    end
   end
 
   def update
@@ -35,6 +33,7 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+      
 
   private
 
@@ -45,5 +44,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user.id
+    else
+      redirect_to root_path
+    end
   end
 end
