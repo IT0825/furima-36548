e88e844a9cf9f27d9200e6a9f16e69a4c6dd9074
IT0825 <ace0829.1114@icloud.com>
2021-08-14 14:32:24 +0900

    修正2点完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -17,19 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == @item.user.id
+    if current_user.id == @item.user.id
     else
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,5 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product_name, :description_of_item, :category_id, :product_condition_id,
                                  :shipping_fee_burden_id, :prefecture_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
