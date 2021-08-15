b8df9916af56182e42960d0e4c07c06e91264d3e
IT0825 <ace0829.1114@icloud.com>
2021-08-15 17:53:02 +0900

    購入画面への遷移と画像などの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def order_params
