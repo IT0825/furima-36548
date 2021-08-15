9959fe6096bcdaa3e10ae21de3194c1fb9ca757a
IT0825 <ace0829.1114@icloud.com>
2021-08-15 15:29:25 +0900

    params設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,14 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @item = Item.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def address_params
+    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(order_id: @order.id)
   end
 end
