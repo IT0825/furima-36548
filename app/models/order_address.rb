f0402aaa3c3136d77178e6c0173c99ce7684856d
IT0825 <ace0829.1114@icloud.com>
2021-08-15 09:59:41 +0900

    Formオブジェクトにバリデーションと保存処理記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,25 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :orders, :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :city
+    validates :block
+    validates :building
+    validates :user_id
+    validates :item_id
+    with_options format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" } do
+      validates :postcode
+    end
+    with_options numericality: { other_than: 0, message: "can't be blank" } do
+      validates :prefecture_id
+    end
+    with_options numericality: { with: /\A\d{10}$|^\d{11}\z/ } do
+      validates :phone_number
+    end
+
+    def save
+      order = Order.create(user_id: user_id, item_id: item_id)
+      Address.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order_id)
+    end
 end
